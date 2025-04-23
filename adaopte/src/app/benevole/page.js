45bb024a05292cc6621dc2d12dad0db337c08b2d
laanatri <laanatri.ade@gmail.com>
2025-04-23 22:00:23 +0200

    add pages benevole and visite

---
+++ adaopte/src/app/benevole/page.js
@@ -0,0 +1,9 @@
+export default function Benevole() {
+    return (
+      <>
+        <h1 className="bg-sky-950">Benevole</h1>
+        <p>c'est la première page</p>
+      </>
+    );
+  }
+   
---
+++ adaopte/src/app/visite/page.js
@@ -0,0 +1,9 @@
+export default function Visite() {
+    return (
+      <>
+        <h1 className="bg-sky-950">Visite</h1>
+        <p>c'est la première page</p>
+      </>
+    );
+  }
+   
