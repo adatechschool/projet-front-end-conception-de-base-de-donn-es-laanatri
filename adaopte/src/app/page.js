adc52608b937921be5357502ba72f5d981baa3a1
laanatri <laanatri.ade@gmail.com>
2025-04-24 13:41:46 +0200

    add cards and buttons

--- adaopte/src/app/page.js
+++ adaopte/src/app/page.js
@@ -1,7 +1,13 @@
-import SearchBar from "./components/organisms/searchbar"
-import Title from "./components/molecules/title"
+import SearchBar from "./components/organisms/searchbar";
+import Title from "./components/molecules/title";
+import Button from "./components/atoms/button";
+import Card from "./components/molecules/card";
 
-import profiles from "../../public/datas/profiles.json"
+
+import Image from 'next/image';
+
+
+import profiles from "../../public/datas/profiles.json";
 
 const textPersonnalite = [
   {
@@ -14,6 +20,60 @@ const textPersonnalite = [
   {
     title: "Prêt⸱e à faire la différence ?",
     paragraphe: "Offrez un peu de votre temps, partagez un moment, créez du lien... Chaque présence compte. En tendant la main à une personne âgée, vous lui offrez bien plus qu'une visite : une vraie bouffée de chaleur humaine."
+  }
+];
+
+const btnProfiles = {
+  id: 1,
+  name: "Voir tous les profils",
+  icon: "",
+  route: "/"
+};
+
+const marcheCards = [
+  {
+    id: 1,
+    icon: "🔍",
+    title: "1. Choisissez une rencontre",
+    paragraphe: "Parcourez les profils de nos aînés et trouvez la personne avec qui vous aimeriez partager un moment chaleureux."
+  },{
+    id: 2,
+    icon: "📝",
+    title: "2. Remplissez le formulaire",
+    paragraphe: "Indiquez vos disponibilités et vos envies. Cela nous aide à organiser une rencontre adaptée et en toute confiance."
+  },
+  {
+    id: 3,
+    icon: "👋",
+    title: "3. Partagez un moment",
+    paragraphe: "Rendez visite à la personne âgée dans un cadre bienveillant. Une discussion, une balade ou simplement l'aider à une tâche ! Chaque moment compte."
+  },
+  {
+    id: 4,
+    icon: "💞",
+    title: "4. Créez du lien",
+    paragraphe: "Si le courant passe, vous pouvez revenir ! Ensemble, tissons des liens durables et combattons l'isolement pas à pas."
+  }
+];
+
+const btnsDifference = [
+  {
+    id: 1,
+    name: "Programmer une visite",
+    icon: "",
+    route: "/"
+  },
+  {
+    id: 2,
+    name: "Faire un don 🫶",
+    icon: "",
+    route: "/"
+  },
+  {
+    id: 3,
+    name: "Devenir bénévole",
+    icon: "",
+    route: "/"
   }
 ];
 
@@ -31,27 +91,35 @@ export default function Home() {
         <SearchBar/>
       </header>
 
-      <section>
+      <section className="section-mosaique">
         <Title text={textPersonnalite[0]}/>
-
         <div className="mosaique">
-
           {profiles.map((profile, index) => (
             <div key={index}>
-              <img src={profile.imageUrl} alt="photo de profile"/>
+              <Image src={profile.imageUrl} width="600" height="600" alt="photo de profile"/>
             </div>
           ))}
-
         </div>
-        
+        <Button link={btnProfiles}/>
       </section>
 
-      <section>
+      <section className="section-marche">
         <Title text={textPersonnalite[1]}/>
+        <div className="marche-cards-content">
+          {marcheCards.map((card) => (
+            <Card text={card} key={card.id}/>
+          ))}
+        </div>
       </section>
 
-      <section>
+      <section className="section-difference">
         <Title text={textPersonnalite[2]}/>
+
+        <div className="difference-buttons-content">
+          {btnsDifference.map((btn) => (
+            <Button link={btn} key={btn.id}/>
+          ))}
+        </div>
       </section>
     </>
   );
