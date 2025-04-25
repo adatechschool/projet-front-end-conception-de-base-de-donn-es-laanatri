b77acce77bc6189141b3a2b7c3b0d6e05a13ff29
laanatri <laanatri.ade@gmail.com>
2025-04-25 13:22:17 +0200

    update

--- adaopte/src/app/visite/page.js
+++ adaopte/src/app/visite/page.js
@@ -1,9 +1,30 @@
+'use client'
+
+import SearchBar from "../components/organisms/searchbar";
+import { useSearchParams } from 'next/navigation';
+
+import ProfileCard from "../components/molecules/profilecard";
+
+import profiles from "../../../public/datas/profiles.json";
+
 export default function Visite() {
-    return (
-      <>
-        <h1 className="bg-sky-950">Visite</h1>
-        <p>c'est la première page</p>
-      </>
-    );
-  }
-   
+  const searchParams = useSearchParams();
+  const activite = searchParams.get('activite');
+  const city = searchParams.get('city').toLowerCase();
+
+  const selectedProfiles = profiles.filter((profile) => profile["city"].toLowerCase() === city && profile["type"] === activite)
+
+  return (
+    <>
+      <section className="section-search">
+        <SearchBar activite={activite} city={city}/>
+      </section>
+
+      <section className="section-results">
+        {selectedProfiles.map((profile, index) => (
+          <ProfileCard props={profile} key={index}/>
+        ))}
+      </section>
+    </>
+  );
+}
